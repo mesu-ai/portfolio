3a154d77eac52e9a57119c7e0ff8216273f5f1a3
mesu-ai <mesu.s.ss@gmail.com>
2021-11-29 22:13:23 +0600

    firebase

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBKD2mgdEPtaqoIJSs0jUBvhQnfJ3kifaM",
+    authDomain: "mesu-portfolio.firebaseapp.com",
+    projectId: "mesu-portfolio",
+    storageBucket: "mesu-portfolio.appspot.com",
+    messagingSenderId: "172029838977",
+    appId: "1:172029838977:web:65d9b6773c2d054cbb2f80",
+    measurementId: "G-4J25EE2BRK"
+  };
+  export default firebaseConfig;
+  

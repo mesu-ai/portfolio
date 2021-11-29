966ae0a425689fb67116ff5e3f8f96470b82af10
mesu-ai <mesu.s.ss@gmail.com>
2021-11-29 21:57:45 +0600

    nav add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Navigation from './components/navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Navigation></Navigation>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+
+
+      </Routes>
+      </BrowserRouter>
+     
     </div>
   );
 }

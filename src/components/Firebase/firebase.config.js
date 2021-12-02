72981507eebff38697d02c9189edc93f9279ba8d
mesu-ai <mesu.s.ss@gmail.com>
2021-12-02 18:42:54 +0600

    firebase config

--- src/components/Firebase/firebase.config.js
+++ src/components/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBKD2mgdEPtaqoIJSs0jUBvhQnfJ3kifaM",
-    authDomain: "mesu-portfolio.firebaseapp.com",
-    projectId: "mesu-portfolio",
-    storageBucket: "mesu-portfolio.appspot.com",
-    messagingSenderId: "172029838977",
-    appId: "1:172029838977:web:65d9b6773c2d054cbb2f80",
-    measurementId: "G-4J25EE2BRK"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
   };
   export default firebaseConfig;
   

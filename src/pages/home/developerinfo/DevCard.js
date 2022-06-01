faccfeabc44f5813c4d3a05d14f6bfc5b0a94ea6
mesu-ai <mesu.s.ss@gmail.com>
2022-06-01 23:17:14 +0600

    update design

--- src/pages/home/developerinfo/DevCard.js
+++ src/pages/home/developerinfo/DevCard.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Card, Col } from 'react-bootstrap';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-const DevCard = () => {
-    return (
-        <div>
+const DevCard = ({info}) => {
+    useEffect(()=>{
+        AOS.init({
             
-        </div>
+        });
+        
+    },[]);
+
+
+    return (
+        <Col>
+        <Card data-aos="zoom-in-up" className="h-100 p-3 shadow-lg " style={{borderRadius:'12px',backgroundColor:'white'}}>
+            <Card.Img  variant="top" src={info.img}  height="40" />
+            <Card.Body>
+            <Card.Title className="text-capitalize fw-bold">{info.title}</Card.Title>
+            <Card.Text style={{textAlign:'justify',width:''}} className=" fs-4 fw-bold text-primary d-flex justify-content-center text-center mt-4">
+                
+                <p className='border border-secondary rounded-circle p-4 text-white' style={{background:'#009444',width:'40%'}}> 
+                 {info.serviceInfo}
+                </p>
+
+                {info.icons && 
+
+                info.icons.map(icon=><>
+                
+                </>)
+                
+                }
+                
+               
+            </Card.Text>
+            </Card.Body>
+        </Card>
+    </Col>
     );
 };
 
--- src/pages/home/developerinfo/DevInfo.js
+++ src/pages/home/developerinfo/DevInfo.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 import DevCard from './DevCard';
+import exp from '../../../images/devinfo/person-hiking-solid.svg'
+import tp from '../../../images/devinfo/list-check-solid.svg'
+import skill from '../../../images/devinfo/code-square.svg'
 
 const devinfos=[
     {
         'id':1,
-        'img':'',
-        'title':'',
-        'serviceInfo':''
+        'img':tp,
+        'title':'Total Projects',
+        'serviceInfo':'12'
     },
     {
-        'id':1,
-        'img':'',
-        'title':'',
-        'serviceInfo':''
+        'id':2,
+        'img':skill,
+        'title':'Skills',
+        'serviceInfo':'11',
+        'icons':[
+            { 
+            'icon1':"fa-brands fa-react",
+            },
+            {'icon1':"bi bi-facebook fs-4"}
+
+        ]
     },
     {
-        'id':1,
-        'img':'',
-        'title':'',
-        'serviceInfo':''
+        'id':3,
+        'img':exp,
+        'title':'Overal Experience',
+        'serviceInfo':'1.5'
     }
 ]
 
@@ -28,11 +38,11 @@ const DevInfo = () => {
         <div style={{backgroundColor:'#f7f7f7',minHeight:'100vh',maxHeight:'auto' }}>
         <Container className="py-5">
         
-        <h3 className="mb-5 fw-bold">My Services</h3>
+        <h3 className="mb-5 fw-bold">Development Information</h3>
         
         <Row xs={1} md={2} lg={3} className="g-4 mt-1">
         {
-        devinfos.map(service=><DevCard></DevCard>)
+        devinfos.map(info=><DevCard info={info} key={info.id}></DevCard>)
         }
 
        </Row>
